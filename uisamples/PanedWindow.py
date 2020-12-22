78a9741500bd973b10eef64677c61bd0537de183
Toshiyuki Arai <t.arai@qolony.co,jp>
2020-12-22 17:41:32 +0900

    Modified the License to be Apache License V2

--- uisamples/PanedWindow.py
+++ uisamples/PanedWindow.py
@@ -1,20 +1,22 @@
+# Copyright 2020-2021 antillia.com Toshiyuki Arai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit,QWidget, QAction, QTabWidget,QVBoxLayout
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
  
-class App(QMainWindow):
- 
-    def __init__(self, name, x, y, width, height):
-        super().__init__()
-        self.setWindowTitle(name)
-        self.setGeometry(x, y, width, height)
- 
-        self.table_widget = PanedWindow(self, width-40, height-40)
-
-        self.setCentralWidget(self.table_widget)
- 
-        self.show()
  
 class PanedWindow(QTabWidget):        
  
@@ -40,11 +42,3 @@ class PanedWindow(QTabWidget):
 
         self.resize(width, height) 
 
-  #def add(self, child, name):
-  #    layout = QVBoxLayout(child)
-  #    self.addTab(child, name)
-       
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App(sys.argv[0], 10, 20, 600, 400)
-    sys.exit(app.exec_())
